896df8278ce355f651be2fd663b43e1ec097f430
Ismael Ruge Gonzalez <ismael.ruge@ithealth.co>
2024-06-13 22:10:20 -0500

    Inicialización Backend Controltotal

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    
+    socket.on('message', (data) => {
+        console.log('Message received on server:', data);
+        io.emit('message', data);
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
